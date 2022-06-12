2c72b6595c1098d44a754f1248d3d50e98cd5cf3
Portia Antwi <portiaantwi257@gmail.com>
2022-06-11 21:57:09 -0700

    commit tree cdd98b69b0f55fb45da045bf4d7907974f0163c6

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2 ;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) 
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max (1,3,6,15,20);
